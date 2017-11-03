8d56f3f8c818ba5a64b522936348fd85168903cc
Mael-Brs <mael.b_53@hotmail.fr>
2017-11-03 15:27:45 +0100

    Add pagination to wine Catalog

--- app/main/controllers/wineCatalog-ctrl.js
+++ app/main/controllers/wineCatalog-ctrl.js
@@ -10,19 +10,43 @@ function WineCatalogCtrl ($log, $scope, $state, WineSearch, $ionicPopup, CacheSe
 
   vm.submit = submit;
   vm.openSearch = openSearch;
+  vm.loadPage = loadPage;
+  vm.page = 0;
+  vm.itemsPerPage = 20;
+  vm.sort = "_score,asc";
+  vm.lastPage = 0;
+  var regExp = new RegExp(/(?:page=)(\d)/);
 
   $scope.$on('$ionicView.enter', function() { 
     vm.query = "";
-    vm.result = {};
+    vm.result = [];
     openSearch();
   });
   
-  function submit(query) {
-    WineSearch.query({query: query}, function(result) {
-        vm.result = result;
+  function submit() {
+    WineSearch.query({
+        query: vm.query,
+        page: vm.page,
+        size: vm.itemsPerPage,
+        sort: null
+    }, function(result, headers) {
+      getLastPage(headers('link'));
+      for (let i = 0; i < result.length; i++) {
+          vm.result.push(result[i]);
+      };
+      $scope.$broadcast('scroll.infiniteScrollComplete');
     }, function(){
         CommonServices.showAlert('error.searchWine');
     });
+  }
+
+  function getLastPage(header){
+    var array = header.split(",");
+    for(var i = 0 ; i < array.length ; i++){
+      if(array[i].indexOf("last") > -1){
+        vm.lastPage = regExp.exec(array[i])[1];
+      }
+    }
   }
 
   vm.createWine = function(id){
@@ -50,12 +74,17 @@ function WineCatalogCtrl ($log, $scope, $state, WineSearch, $ionicPopup, CacheSe
               //don't allow the user to close unless he enters an input
               e.preventDefault();
             } else {
-              vm.submit(vm.query);
+              vm.submit();
             }
           }
         }
       ]
     });
+  }
+
+  function loadPage(page) {
+    vm.page = page;
+    vm.submit();
   }
 
 }
